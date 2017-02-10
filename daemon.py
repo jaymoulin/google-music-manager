8818cca9745258d3cfe25a6096fce3037d3bd051
Jay MOULIN <jaymoulin@gmail.com>
2017-02-10 17:05:31 +0100

    Fix logs

--- daemon.py
+++ daemon.py
@@ -11,18 +11,19 @@ from gmusicapi import Musicmanager
 
 class MusicToUpload(FileSystemEventHandler):
     def on_created(self,event):
-        logging.info("Uploading "+event.src_path)
+        self.logger.info("Uploading "+event.src_path)
         self.api.upload(event.src_path, True)
 
 if __name__ == "__main__":
-    FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-    logging.basicConfig(format=FORMAT)
-    logging.info("Init Daemon - Press Ctrl+C to quit")
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info("Init Daemon - Press Ctrl+C to quit")
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    oauth = sys.argv[2] if len(sys.argv) >= 2 else '/root/oauth'
+    oauth = sys.argv[2] if len(sys.argv) > 2 else '/root/oauth'
     api = Musicmanager()
     event_handler = MusicToUpload()
     event_handler.api = api
+    event_handler.logger = logger
     if api.login(oauth) != False:
         observer = Observer()
         observer.schedule(event_handler, path, recursive=True)
